0a32e8c5d9e5ea80757293a07b10315c2b6d012a
Ethan <ethan@EthanuicBookPro.playio.kr>
2024-11-20 17:08:31 +0900

    [공통] 은행, 계좌 vo 정의 및 더미데이터 생성

---
+++ lib/screen/main/tab/home/banks_dummy.dart
@@ -0,0 +1,6 @@
+import 'package:toss_app/common/constants.dart';
+import 'package:toss_app/screen/main/tab/home/vo/vo_bank.dart';
+
+final bankShinhan = Bank('신한은행', '$basePath/bank/bank_shinhan.png');
+final bankKakao = Bank('카카오뱅그', '$basePath/bank/bank_kakao.png');
+final bankToss = Bank('토스뱅크', '$basePath/bank/bank_toss.png');
